c515f442992d673d9d3e32a145f7353b933f259b
walterLaborde <walter.laborde@hotmail.com>
2025-11-20 09:48:01 -0300

    Create Testimonial service

---
+++ src/main/java/com/api/csm/services/TestimonialService.java
@@ -0,0 +1,72 @@
+package com.api.csm.services;
+
+import com.api.csm.dto.CreateTestimonialRequest;
+import com.api.csm.models.*;
+import com.api.csm.repository.*;
+import com.api.csm.utils.MediaType;
+import com.api.csm.utils.TestimonialStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class TestimonialService {
+
+    private final TestimonialRepository testimonialRepository;
+    private final CategoryRepository categoryRepository;
+    private final TagRepository tagRepository;
+    private final MediaRepository mediaRepository;
+    private final UserRepository userRepository;
+
+    public Testimonial createTestimonial(CreateTestimonialRequest request, UUID userId){
+
+        User user;
+        user= userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado."));
+
+        //Obtener categories
+        List<Category> categories = categoryRepository.findAllById(
+                request.getCategories().stream()
+                        .map(UUID::fromString)
+                        .collect(Collectors.toList())
+        );
+
+        //Obtener tags
+        List<Tag> tags = tagRepository.findAllById(
+                request.getTags().stream()
+                        .map(UUID::fromString)
+                        .collect(Collectors.toList())
+        );
+
+        Testimonial testimonial = Testimonial.builder()
+                .title(request.getTitle())
+                .content(request.getContent())
+                .status(TestimonialStatus.PENDING)
+                .createdBy(user)
+                .tags(tags)
+                .build();
+
+        testimonialRepository.save(testimonial);
+
+        //Guardar medias
+        if(request.getMediaUrls()!=null){
+            List<Media> mediaList = request.getMediaUrls().stream()
+                    .map(url -> Media.builder()
+                            .url(url)
+                            .type(MediaType.IMAGE)
+                            .testimonial(testimonial)
+                            .build())
+                    .collect(Collectors.toList());
+
+            mediaRepository.saveAll(mediaList);
+
+            testimonial.setMedia(mediaList);
+        }
+
+        return testimonialRepository.save(testimonial);
+    }
+}
