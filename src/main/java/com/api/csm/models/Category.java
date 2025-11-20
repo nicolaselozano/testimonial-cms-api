9288030feb9aa43f8f6d8402eb81c3af2eab09e0
walterLaborde <walter.laborde@hotmail.com>
2025-11-20 09:41:01 -0300

    Create Category Entity

---
+++ src/main/java/com/api/csm/models/Category.java
@@ -0,0 +1,24 @@
+package com.api.csm.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true,nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+}
