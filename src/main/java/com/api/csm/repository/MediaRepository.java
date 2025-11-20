08247523c4c13eda11bffc4a19735140fa03583a
walterLaborde <walter.laborde@hotmail.com>
2025-11-20 09:44:33 -0300

    Create Media Repository

---
+++ src/main/java/com/api/csm/repository/MediaRepository.java
@@ -0,0 +1,13 @@
+package com.api.csm.repository;
+
+import com.api.csm.models.Media;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface MediaRepository extends JpaRepository<Media, UUID> {
+
+    //Para buscar por url
+    Optional<Media> findByUrl(String url);
+}
