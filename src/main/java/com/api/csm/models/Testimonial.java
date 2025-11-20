f1e5b80cc106cbad95a271b1918cf397d588878e
walterLaborde <walter.laborde@hotmail.com>
2025-11-20 09:37:13 -0300

    Create Class Testimonial

--- src/main/java/com/api/csm/models/Testimonial.java
+++ src/main/java/com/api/csm/models/Testimonial.java
@@ -1,5 +1,6 @@
-package com.team28.testimonialcmsapi.model;
+package com.api.csm.models;
 
+import com.api.csm.utils.TestimonialStatus;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,7 +17,7 @@ import lombok.*;
 public class Testimonial {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String title;
@@ -25,19 +26,33 @@ public class Testimonial {
     private String content;
 
     @Enumerated(EnumType.STRING)
-    private testimonialStatus status;
+    private TestimonialStatus status;
 
-    @ManyToMany
+    @ManyToOne
     @JoinColumn(name = "user_id")
     private User createdBy;
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
+    @ManyToOne
     @JoinTable(
             name = "testimonial_category",
+            joinColumns = @JoinColumn(name = "testimonial_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private List<Category> categories;
+
+    @ManyToMany
+    @JoinTable(
+            name = "testimonial_tag",
             joinColumns = @JoinColumn(name = "testimonial_id"),
             inverseJoinColumns = @JoinColumn(name = "tag_id")
     )
     private List<Tag> tags;
+
+    @OneToMany(mappedBy = "testimonial", cascade = CascadeType.ALL)
+    private List<Media> media;
+
+
 }
