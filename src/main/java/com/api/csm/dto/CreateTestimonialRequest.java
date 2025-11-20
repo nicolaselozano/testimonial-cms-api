2a1bc3674eaed8bca3d343b92747a6dc4049106d
walterLaborde <walter.laborde@hotmail.com>
2025-11-20 09:39:42 -0300

    Create Testimonial Request DTO

---
+++ src/main/java/com/api/csm/dto/CreateTestimonialRequest.java
@@ -0,0 +1,29 @@
+package com.api.csm.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.apache.catalina.LifecycleState;
+
+import java.util.List;
+
+@Data
+
+public class CreateTestimonialRequest {
+
+    @NotBlank
+    @Size(max = 255)
+    private String title;
+
+    @NotBlank
+    private String content;
+
+    //Lista de urls de media
+    private List<String> mediaUrls;
+
+    //Lista de los ids de categories y tags
+    private List<String> categories;
+
+    private List<String> tags;
+}
