da61b6e05b71cba0c7d17391727aa05eb71e535d
walterLaborde <walter.laborde@hotmail.com>
2025-11-21 19:35:16 -0300

    Change GenerationType in Tag

--- src/main/java/com/api/csm/models/Tag.java
+++ src/main/java/com/api/csm/models/Tag.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 public class Tag {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(unique = true,nullable = false)
