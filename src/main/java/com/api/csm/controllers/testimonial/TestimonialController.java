acdf262ff011610f460a89663becb1b3c7638759
walterLaborde <walter.laborde@hotmail.com>
2025-11-21 19:31:19 -0300

    Change output type TestimonialController

--- src/main/java/com/api/csm/controllers/testimonial/TestimonialController.java
+++ src/main/java/com/api/csm/controllers/testimonial/TestimonialController.java
@@ -1,13 +1,14 @@
 package com.api.csm.controllers.testimonial;
 
-import com.api.csm.dto.CreateTestimonialRequest;
+import com.api.csm.dto.TestimonialRequestDTO;
+import com.api.csm.dto.TestimonialResponseDTO;
 import com.api.csm.models.Testimonial;
 import com.api.csm.services.TestimonialService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.Authentication;
 
-import java.security.Principal;
 import java.util.UUID;
 
 @RestController
@@ -18,13 +19,13 @@ public class TestimonialController {
     private final TestimonialService testimonialService;
 
     @PostMapping
-    public ResponseEntity<Testimonial> create(
-            @RequestBody CreateTestimonialRequest request,
-            Principal principal
+    public ResponseEntity<TestimonialResponseDTO> create(
+            @RequestBody TestimonialRequestDTO request,
+            Authentication authentication
     ){
-        UUID userId = UUID.fromString(principal.getName());
+        UUID userId = UUID.fromString(authentication.getName());
 
-        Testimonial created = testimonialService.createTestimonial(request,userId);
+        TestimonialResponseDTO created = testimonialService.createTestimonial(request,userId);
 
         return ResponseEntity.ok(created);
     }
