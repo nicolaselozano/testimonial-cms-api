5aa66e2fb51e8070e1ca6aa4740cc4e4d705fccf
walterLaborde <walter.laborde@hotmail.com>
2025-11-21 19:39:14 -0300

    Added fields to build

--- src/main/java/com/api/csm/services/TestimonialService.java
+++ src/main/java/com/api/csm/services/TestimonialService.java
@@ -1,6 +1,8 @@
 package com.api.csm.services;
 
-import com.api.csm.dto.CreateTestimonialRequest;
+import com.api.csm.dto.TestimonialRequestDTO;
+import com.api.csm.dto.TestimonialResponseDTO;
+import com.api.csm.mapper.TestimonialMapper;
 import com.api.csm.models.*;
 import com.api.csm.repository.*;
 import com.api.csm.utils.MediaType;
@@ -8,6 +10,7 @@ import com.api.csm.utils.TestimonialStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -21,9 +24,9 @@ public class TestimonialService {
     private final TagRepository tagRepository;
     private final MediaRepository mediaRepository;
     private final UserRepository userRepository;
+    private final TestimonialMapper mapper;
 
-    public Testimonial createTestimonial(CreateTestimonialRequest request, UUID userId){
-
+    public TestimonialResponseDTO createTestimonial(TestimonialRequestDTO request, UUID userId){
         User user;
         user= userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("Usuario no encontrado."));
@@ -47,7 +50,10 @@ public class TestimonialService {
                 .content(request.getContent())
                 .status(TestimonialStatus.PENDING)
                 .createdBy(user)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
                 .tags(tags)
+                .categories(categories)
                 .build();
 
         testimonialRepository.save(testimonial);
@@ -67,6 +73,6 @@ public class TestimonialService {
             testimonial.setMedia(mediaList);
         }
 
-        return testimonialRepository.save(testimonial);
+        return mapper.toResponseDTO(testimonial);
     }
 }
