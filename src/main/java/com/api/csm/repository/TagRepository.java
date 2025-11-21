eb1a83b530576b8385baaef251957a47a8a268bb
walterLaborde <walter.laborde@hotmail.com>
2025-11-21 19:37:43 -0300

    Create tag repository

--- src/main/java/com/api/csm/repository/TagRepository.java
+++ src/main/java/com/api/csm/repository/TagRepository.java
@@ -4,6 +4,8 @@ import com.api.csm.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
+import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, UUID> {
+    Optional<Tag> findByName(String name);
 }
